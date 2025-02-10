4c8057fadd1b260d893d37e4d7efcba0d94dc4a8
sagar <sagaryadav12369@gmail.com>
2025-02-10 22:41:11 +0530

    plant app

---
+++ app/src/main/java/com/example/shopy/ui/activity/LogInActivity.kt
@@ -0,0 +1,30 @@
+package com.example.shopy.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.shopy.MainActivity
+import com.example.shopy.R
+import com.example.shopy.databinding.ActivityLogInBinding
+
+class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        binding.loginbtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shopy/ui/activity/StartActivity.kt
@@ -0,0 +1,27 @@
+package com.example.shopy.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.shopy.databinding.ActivityStartBinding
+
+
+class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        binding.loginbtn.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
